fd860f0a2f61744cd65bb7ba83087e9a5442ae9e
Udith <udithshalinda2@gmail.com>
2019-09-12 12:22:31 +0530

    flutter build.gradle error fixed

---
+++ lib/ui/scan.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Scan extends StatefulWidget {
+  @override
+  _ScanState createState() => _ScanState();
+}
+
+class _ScanState extends State<Scan> {
+  File _image;
+  
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = image;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+      ),
+      body: Center(
+        child: _image == null
+            ? Text('No image selected.')
+            : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
