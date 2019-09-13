30fb88bed02e487d65cee0d81f032f7b80d8c04c
Udith <udithshalinda2@gmail.com>
2019-09-13 19:55:34 +0530

    update

--- lib/ui/scan.dart
+++ lib/ui/scan.dart
@@ -1,24 +1,32 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
+
+
+bool useFlutterImageCompress = true;
+bool keepExif = true;
+bool autoCorrectionAngle = true;
+
+
 class Scan extends StatefulWidget {
+
   @override
   _ScanState createState() => _ScanState();
 }
 
 class _ScanState extends State<Scan> {
   File _image;
-  final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
-  final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(imageFile);
-  
+
+
   Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
-    print(image.path);
+    File imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
     setState(() {
-      _image = image;
+      _image = imageFile;
     });
+    print(_image.path);
   }
   
   @override
@@ -27,10 +35,16 @@ class _ScanState extends State<Scan> {
       appBar: AppBar(
         title: Text('Image Picker Example'),
       ),
-      body: Center(
-        child: _image == null
-            ? Text('No image selected.')
-            : Image.file(_image),
+      body: Column(
+        children: <Widget>[
+          _image == null
+              ? Text('No image selected.')
+              : Image.file(_image),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: _initializeVision,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: getImage,
@@ -39,4 +53,33 @@ class _ScanState extends State<Scan> {
       ),
     );
   }
+  void _initializeVision() async {
+
+    // create vision image from that file
+    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(this._image);
+    // create detector index
+    final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
+
+    // find text in image
+
+    final VisionText visionText = await textRecognizer.processImage(visionImage);
+    print(visionText);
+
+    // got the pattern from that SO answer: https://stackoverflow.com/questions/16800540/validate-email-address-in-dart
+//    String mailPattern =
+//        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+//    RegExp regEx = RegExp(mailPattern);
+//
+//    String mailAddress =
+//        "Couldn't find any mail in the foto! Please try again!";
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+//        if (regEx.hasMatch(line.text)) {
+//          mailAddress = line.text;
+          print(line.text);
+//        }
+      }
+    }
+  }
+
 }
